02c2125bbc9721bb540ac432960b058d93315649
westerndundrey <hijackedpuffin@gmail.com>
2024-07-22 18:08:43 +0800

    Beam

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Beam:
+    def __init__(self,length):
+        self.length = length
+        self.loads = []
+
+def add_point_load(self, magnitude, position):
